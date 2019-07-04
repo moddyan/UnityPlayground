db1579b975fdd13f0874c2af001b296acc416d8e
moddyan <moddyan@gmail.com>
2019-07-05 01:41:04 +0800

    1.4 20%

---
+++ Assets/1.4_fractal/Fractal.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fractal : MonoBehaviour
+{
+    public Mesh mesh;
+    public Material material;
+
+    public int maxDepth;
+    public float childScale;
+
+    private int depth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.AddComponent<MeshFilter>().mesh = mesh;
+        gameObject.AddComponent<MeshRenderer>().material = material;
+        if (depth < maxDepth)
+        {
+            new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this);
+        }
+    }
+
+    private void Initialize(Fractal parent)
+    {
+        mesh = parent.mesh;
+        material = parent.material;
+        maxDepth = parent.maxDepth;
+        depth = parent.depth + 1;
+        childScale = parent.childScale;
+        transform.parent = parent.transform;
+        transform.localScale = Vector3.one * childScale;
+        transform.localPosition = Vector3.up * (0.5f + 0.5f * childScale);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
