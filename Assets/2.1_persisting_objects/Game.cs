3f1af5cefe84cf6b94417ab3bfe2cc3b8758594d
moddyan <moddyan@gmail.com>
2019-07-25 00:26:02 +0800

    2.1 done

---
+++ Assets/2.1_persisting_objects/Game.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Game : PersistableObject
+{
+    public PersistableObject prefab;
+
+    public PersistentStorage storage;
+
+    List<PersistableObject> objects;
+
+    public KeyCode createKey = KeyCode.C;
+    public KeyCode newGameKey = KeyCode.N;
+    public KeyCode saveKey = KeyCode.S;
+    public KeyCode loadKey = KeyCode.L;
+
+    private void Awake()
+    {
+        objects = new List<PersistableObject>();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(createKey))
+        {
+            CreateObject();
+        }
+        else if (Input.GetKey(newGameKey))
+        {
+            BeginNewGame();
+        }
+        else if (Input.GetKeyDown(saveKey))
+        {
+            storage.Save(this);
+        }
+        else if (Input.GetKeyDown(loadKey))
+        {
+            BeginNewGame();
+            storage.Load(this);
+        }
+    }
+
+    void BeginNewGame()
+    {
+        for (int i = 0; i < objects.Count; i++)
+        {
+            Destroy(objects[i].gameObject);
+        }
+        objects.Clear();
+    }
+
+
+    void CreateObject()
+    {
+        PersistableObject c = Instantiate(prefab);
+        Transform t = c.transform;
+        t.localPosition = Random.insideUnitSphere * 5f;
+        t.localRotation = Random.rotation;
+        t.localScale = Vector3.one * Random.Range(0.1f, 1f);
+        objects.Add(c);
+    }
+
+    public override void Save(GameDataWriter writer)
+    {
+        writer.Write(objects.Count);
+        for (int i = 0; i < objects.Count; i++)
+        {
+            objects[i].Save(writer);
+        }
+    }
+
+    public override void Load(GameDataReader reader)
+    {
+        int count = reader.ReadInt();
+        for (int i = 0; i < count; i++)
+        {
+            PersistableObject o = Instantiate(prefab);
+            o.Load(reader);
+            objects.Add(o);
+        }
+    }
+}
