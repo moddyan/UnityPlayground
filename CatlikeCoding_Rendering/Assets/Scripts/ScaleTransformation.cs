7e26cc16bd048e1f5bef5036c95c3ae40f56ffea
yanyangjiang <yanyangjiang@topjoy.com>
2019-11-22 23:12:08 +0800

    rendering/part1/section2

---
+++ CatlikeCoding_Rendering/Assets/Scripts/ScaleTransformation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleTransformation : Transformation
+{
+    public Vector3 scale = new Vector3(1.0f, 1.0f, 1.0f);
+
+    public override Vector3 Apply(Vector3 point)
+    {
+        point.x *= scale.x;
+        point.y *= scale.y;
+        point.z *= scale.z;
+        return point;
+    }
+}
