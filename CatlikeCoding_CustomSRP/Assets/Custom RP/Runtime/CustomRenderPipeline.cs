d056c3d1398babfd89c310a1b47cddbdf350f770
moddyan <moddyan@gmail.com>
2021-04-08 00:49:51 +0800

    SRP.1  part 1

---
+++ CatlikeCoding_CustomSRP/Assets/Custom RP/Runtime/CustomRenderPipeline.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CustomRenderPipeline : RenderPipeline
+{
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        
+    }
+}
