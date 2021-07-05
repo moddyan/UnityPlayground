62e572f7d9101ffd0e0b6f3e738c5f135cfcd498
moddyan <moddyan@gmail.com>
2021-07-06 01:25:33 +0800

    SRP.4 1.8

--- CatlikeCoding_CustomSRP/Assets/Custom RP/Runtime/Shadows.cs
+++ CatlikeCoding_CustomSRP/Assets/Custom RP/Runtime/Shadows.cs
@@ -55,10 +55,33 @@ public class Shadows
         int atlasSize = (int) settings.directional.atlasSize;
         buffer.GetTemporaryRT(dirShadowAtlasId, atlasSize, atlasSize,
             32, FilterMode.Bilinear, RenderTextureFormat.Shadowmap);
-        buffer.SetRenderTarget(dirShadowAtlasId, 
+        buffer.SetRenderTarget(dirShadowAtlasId,
             RenderBufferLoadAction.DontCare, RenderBufferStoreAction.Store);
         buffer.ClearRenderTarget(true, false, Color.clear);
+        buffer.BeginSample(bufferName);
         ExecuteBuffer();
+
+        for (int i = 0; i < shadowedDirectionalLightCount; i++)
+        {
+            RenderDirectionalShadows(i, atlasSize);
+        }
+
+        buffer.EndSample(bufferName);
+        ExecuteBuffer();
+    }
+
+    void RenderDirectionalShadows(int index, int tileSize)
+    {
+        ShadowedDirectionalLight light = ShadowedDirectionalLights[index];
+        var shadowSettings = new ShadowDrawingSettings(cullingResults, light.visibleLightIndex);
+        cullingResults.ComputeDirectionalShadowMatricesAndCullingPrimitives(
+            light.visibleLightIndex, 0, 1, Vector3.zero, tileSize, 0f,
+            out Matrix4x4 viewMatrix, out Matrix4x4 projectionMatrix,
+            out ShadowSplitData splitData);
+        shadowSettings.splitData = splitData;
+        buffer.SetViewProjectionMatrices(viewMatrix, projectionMatrix);
+        ExecuteBuffer();
+        context.DrawShadows(ref shadowSettings);
     }
 
     void ExecuteBuffer()
--- CatlikeCoding_CustomSRP/Assets/Custom RP/Shaders/Lit.shader
+++ CatlikeCoding_CustomSRP/Assets/Custom RP/Shaders/Lit.shader
@@ -6,7 +6,7 @@ Shader "Custom RP/Lit"
         _BaseColor("Color", Color) = (0.5, 0.5, 0.5, 1.0)
         _Cutoff ("Alpha Cutoff", Range(0.0, 1.0)) = 0.5
         [Toggle(_CLIPPING)] _Clipping ("Alpha Clipping", Float) = 0
-        [Toggle(_PREMULTIPLY_ALPHA)] _PremulAlpha  ("Premultiply Alpha", Float) = 0
+        [Toggle(_PREMULTIPLY_ALPHA)] _PremulAlpha ("Premultiply Alpha", Float) = 0
 
         _Metallic ("Metallic", Range(0, 1)) = 0
         _Smoothness ("Smoothness", Range(0, 1)) = 0.5
@@ -38,6 +38,24 @@ Shader "Custom RP/Lit"
 
             #include "LitPass.hlsl"
             ENDHLSL
+        }
+
+        Pass
+        {
+            Tags
+            {
+                "LightMode" = "ShadowCaster"
+            }
+            ColorMask 0
+
+			HLSLPROGRAM
+			#pragma target 3.5
+			#pragma shader_feature _CLIPPING
+			#pragma multi_compile_instancing
+			#pragma vertex ShadowCasterPassVertex
+			#pragma fragment ShadowCasterPassFragment
+			#include "ShadowCasterPass.hlsl"
+			ENDHLSL
         }
     }
     CustomEditor "CustomShaderGUI"
